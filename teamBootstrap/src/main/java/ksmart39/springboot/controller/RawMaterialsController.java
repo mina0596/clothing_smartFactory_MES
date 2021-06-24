a6cc22b9fbb66b9d8d69fc2bbc048b95ae2a713a
Minah Park <mina0596@hotmail.com>
2021-06-25 04:06:15 +0900

    [tema1] 민아정사항 author: 박민아 file: [생성] searchMaterialName.html [수정] RawMaterialController.java addRawMaterials.html RawMaterialsService.java detail:모르고 develop에 작업.....

--- teamBootstrap/src/main/java/ksmart39/springboot/controller/RawMaterialsController.java
+++ teamBootstrap/src/main/java/ksmart39/springboot/controller/RawMaterialsController.java
@@ -1,14 +1,52 @@
 package ksmart39.springboot.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import ksmart39.springboot.service.RawMaterialsService;
 
 @Controller
 public class RawMaterialsController {
 	
-
+	private static final Logger log = LoggerFactory.getLogger(RawMaterialsController.class);
+	
+	@Autowired
+	private RawMaterialsService rawMaterialsService;
+	
+	//[민아]원부자재 재료 구분 검색후 처리
+	@PostMapping("/searchRawMaterialName")
+	@ResponseBody
+	public String sendMaterialName(@RequestParam(name = "materialName", required = false) String materialName
+								  , Model model) {
+		
+		log.info("MaterialNameCheck 	materialName :::::: {}", materialName);
+		model.addAttribute("materialName", materialName);
+		return "redirect:/addRawMaterials";
+	}
+	
+	
+	//[민아]원부자재 재료 구분 검색
+	@GetMapping("/searchMaterialName")
+	public String getSearchValue() {
+		//여기에 list DB에서 받아서 뿌려줄꺼임
+		return "rawMaterials/searchMaterialName";
+	}
+	
+	//[민아]원부자재 등록화면
+	@GetMapping("/addRawMaterials")
+	public String addRawMeterials(Model model) {
+		
+		return "rawMaterials/addRawMaterials";
+	}
 	
 	//[한빛]출고현황
 	@GetMapping("/exWarehousingList")
@@ -24,6 +62,7 @@ public class RawMaterialsController {
 	//[한빛]소요별 자재 정보 입력 후 자재 입고 목록으로 리다이렉트 , 파라미터는 임시 값
 	@PostMapping("/addMaterialsUse")
 	public String addExWarehousing(@RequestParam(value = "raw_material_name", required = false )String raw_material_name) {
+		
 		return "redirect:/materialsUseList";
 	}
 	//[한빛]소요별 자재조회/목록
@@ -38,12 +77,6 @@ public class RawMaterialsController {
 		return "rawMaterials/addMaterialsUse";
 	}
 	
-	//[민아]원부자재 등록화면
-	@GetMapping("/addRawMaterials")
-	public String addRawMeterials() {
-		return "rawMaterials/addRawMaterials";
-	}
-		
 	//[민아]원부자재 목록
 	@GetMapping("/rawMaterialsList")
 	public String getRawMeterialsList() {
