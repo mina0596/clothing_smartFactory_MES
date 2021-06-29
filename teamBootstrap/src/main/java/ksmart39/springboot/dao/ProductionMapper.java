0a24c5faab221e3274fb72705b88cec5e1296726
ECS <ECS@DESKTOP-048HU3O>
2021-06-29 10:07:20 +0900

    [team1] 다미 수정사항 author: 김다미 file: [생성]dao/ProductionMapper.java [생성]mapper/ProductionMapper.xml [생성]service/ProductionService.java [생성]productionDailyPlanList.html [생성]productionMonthlyPlanList.html [생성]productionWeeklyPlanList.html [수정]controller/ProductionController.java [수정]fragments/sidebar_production.html [수정]fragments/head.html  fullcalendar lib 추가 teamBootstrap/src/main/resources/static/Dashio/fullcalendar/demo-tocodepen.css teamBootstrap/src/main/resources/static/Dashio/fullcalendar/demo-to-codepen.js teamBootstrap/src/main/resources/static/Dashio/fullcalendar/main.css teamBootstrap/src/main/resources/static/Dashio/fullcalendar/main.js teamBootstrap/src/main/resources/static/Dashio/fullcalendar/main.min.css teamBootstrap/src/main/resources/static/Dashio/fullcalendar/main.min.js detail: fullcalendar lib, css 추가 		월별, 일별, 주간별 생산계획 html 추가

--- teamBootstrap/src/main/java/ksmart39/springboot/dao/ProductionMapper.java
+++ teamBootstrap/src/main/java/ksmart39/springboot/dao/ProductionMapper.java
@@ -1,5 +1,14 @@
 package ksmart39.springboot.dao;
 
-public interface ProductionMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import ksmart39.springboot.domain.ProductionPlan;
+
+@Mapper
+public interface ProductionMapper {
+	
+	//[다미]생산계획 조회
+	public List<ProductionPlan> getProductionMonthlyPlanList();
 }
