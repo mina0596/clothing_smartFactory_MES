177984752916f917b02c82e01890305fe12d80e3
Minah Park <mina0596@hotmail.com>
2021-07-06 23:40:39 +0900

    [team01]민아 수정사항
    details:
    -각 헤더의 버튼을 눌렀을때의 메인화면
    -원부자재리스트 화면 system에서

---
+++ teamBootstrap/src/main/java/ksmart39/springboot/domain/RawMaterials.java
@@ -0,0 +1,98 @@
+package ksmart39.springboot.domain;
+
+public class RawMaterials {
+	private String rawMaterialCode;
+	private String highClass;
+	private String medClass;
+	private String medCode;
+	private String rawMaterialName;
+	private String subCode;
+	private String colorCode;
+	private String feature;
+	private String unit;
+	private String rawMaterialRegDate;
+	private String rawMaterialUpdateDate;
+	private int num;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getRawMaterialCode() {
+		return rawMaterialCode;
+	}
+	public void setRawMaterialCode(String rawMaterialCode) {
+		this.rawMaterialCode = rawMaterialCode;
+	}
+	public String getHighClass() {
+		return highClass;
+	}
+	public void setHighClass(String highClass) {
+		this.highClass = highClass;
+	}
+	public String getMedClass() {
+		return medClass;
+	}
+	public void setMedClass(String medClass) {
+		this.medClass = medClass;
+	}
+	public String getMedCode() {
+		return medCode;
+	}
+	public void setMedCode(String medCode) {
+		this.medCode = medCode;
+	}
+	public String getRawMaterialName() {
+		return rawMaterialName;
+	}
+	public void setRawMaterialName(String rawMaterialName) {
+		this.rawMaterialName = rawMaterialName;
+	}
+	public String getSubCode() {
+		return subCode;
+	}
+	public void setSubCode(String subCode) {
+		this.subCode = subCode;
+	}
+	public String getColorCode() {
+		return colorCode;
+	}
+	public void setColorCode(String colorCode) {
+		this.colorCode = colorCode;
+	}
+	public String getFeature() {
+		return feature;
+	}
+	public void setFeature(String feature) {
+		this.feature = feature;
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	public String getRawMaterialRegDate() {
+		return rawMaterialRegDate;
+	}
+	public void setRawMaterialRegDate(String rawMaterialRegDate) {
+		this.rawMaterialRegDate = rawMaterialRegDate;
+	}
+	public String getRawMaterialUpdateDate() {
+		return rawMaterialUpdateDate;
+	}
+	public void setRawMaterialUpdateDate(String rawMaterialUpdateDate) {
+		this.rawMaterialUpdateDate = rawMaterialUpdateDate;
+	}
+	@Override
+	public String toString() {
+		return "RawMaterials [rawMaterialCode=" + rawMaterialCode + ", highClass=" + highClass + ", medClass="
+				+ medClass + ", medCode=" + medCode + ", rawMaterialName=" + rawMaterialName + ", subCode=" + subCode
+				+ ", colorCode=" + colorCode + ", feature=" + feature + ", unit=" + unit + ", rawMaterialRegDate="
+				+ rawMaterialRegDate + ", rawMaterialUpdateDate=" + rawMaterialUpdateDate + ", num=" + num + "]";
+	}
+
+	
+	
+}
