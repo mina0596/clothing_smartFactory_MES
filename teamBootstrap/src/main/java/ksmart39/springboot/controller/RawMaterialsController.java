4e32c08c7741955ebec70b569721ae68e1468d3f
MinahPark <mina0596@hotmail.com>
2021-07-08 14:23:55 +0900

    [team01] 쌤한테 물어보기전

--- teamBootstrap/src/main/java/ksmart39/springboot/controller/RawMaterialsController.java
+++ teamBootstrap/src/main/java/ksmart39/springboot/controller/RawMaterialsController.java
@@ -1,7 +1,9 @@
 package ksmart39.springboot.controller;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import ksmart39.springboot.domain.RawMaterials;
 import ksmart39.springboot.domain.RawMaterialsInventory;
 import ksmart39.springboot.service.RawMaterialsInventoryStatusService;
 import ksmart39.springboot.service.RawMaterialsService;
@@ -66,6 +69,11 @@ public class RawMaterialsController {
 	public String addExWarehousing() {
 		return "rawMaterials/addExWarehousing";
 	}	
+	//[민아]출고내역 수정
+	@GetMapping("/modifyExWarehousing")
+	public String modifyExWarehousing() {
+		return "rawMaterials/modifyExWarehousing";
+	}	
 	
 	//[한빛]소요별 자재 정보 입력 후 자재 입고 목록으로 리다이렉트 , 파라미터는 임시 값
 	@PostMapping("/addMaterialsUse")
@@ -87,12 +95,37 @@ public class RawMaterialsController {
 	
 
 	//===================================================================
-	//[다미]자재입고 수정
+	//[다미]자재입고 수정 + [민아] 자재입고현황에서 수정화면으로 넘어갈때 상황 추가
 	@GetMapping("/modifyInWarehousing")
-	public String modifyInWarehousing(@RequestParam(value = "raw_material_name", required = false)String raw_material_name) {
+	public String modifyInWarehousing(@RequestParam(value = "materialsName", required = false)String materialsName
+									 ,@RequestParam(value = "materialsCode", required = false)String materialsCode
+									 ,@RequestParam(value = "materialsInAmount", required = false)String materialsInAmount
+									 ,String transactionCate
+									 ,Model model) {
+		
+		model.addAttribute("materialsCode", materialsCode);
+		model.addAttribute("materialsInAmount", materialsInAmount);
+		model.addAttribute("materialsName", materialsName);
+		
+		
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("materialsName", materialsName);
+		paramMap.put("materialsCode", materialsCode);
+		paramMap.put("materialsInAmount", materialsInAmount);
+		paramMap.put("transactionCate", "입고");
+		
+		log.info("-----------------------------");
+		log.info("paramMap :{}", paramMap);
+		
+		List<RawMaterialsInventory> InventoryInfoByMCode = materialsInventoryStatusService.getInventoryInfoByMCode(paramMap);
+		model.addAttribute("InventoryInfoByMCode", InventoryInfoByMCode);
+		
+		log.info("materialsCode = {} ", materialsCode);
+		log.info("materialsInAmount = {} ", materialsInAmount);
+
 		return "rawMaterials/modifyInWarehousing";
 	}
-		//TO[다미] MATERIALS 빼주세용 METHOD명에, 내가 파일이름이랑 다른것들은 바꿧어요 FROM[민아]
+
 	//[다미]자재입고 리스트
 	@GetMapping("/inWarehousingList")
 	public String InWarehousingList() {
