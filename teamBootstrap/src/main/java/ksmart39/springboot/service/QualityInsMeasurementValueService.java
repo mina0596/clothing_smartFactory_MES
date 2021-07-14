8ce3a2b08c38b9b1b66ff68f1212c5f47b14b1c2
ECS <ECS@DESKTOP-048HU3O>
2021-07-14 16:41:23 +0900

    [team1] 다미 수정 : 품질관리 dto 생성, 검색 후 행 생성 [auther] 김다미 [file] teamBootstrap/src/main/resources/templates/quality/addInspectionMeasurementValue.html teamBootstrap/bin/src/main/resources/static/quality/addInspectionMeasurementValue.js teamBootstrap/src/main/resources/static/quality/addInspectionMeasurementValue.js teamBootstrap/src/main/resources/templates/quality/addInspectionMeasurementValueMenu/modal_addInspectionMeasurementValue.html teamBootstrap/src/main/java/ksmart39/springboot/domain/QualityBiochemFabricLevelStandard.java teamBootstrap/src/main/java/ksmart39/springboot/controller/QualityControlController_KDM.java teamBootstrap/src/main/java/ksmart39/springboot/dao/QualityInsMeasurementValueMapper.java teamBootstrap/src/main/java/ksmart39/springboot/service/QualityInsMeasurementValueService.java teamBootstrap/src/main/resources/mapper/QualityInspectionMeasurementValue.xml teamBootstrap/src/main/java/ksmart39/springboot/domain/QualityInspectionResult.java teamBootstrap/src/main/java/ksmart39/springboot/domain/QualityInspectionStandard.java [detail] 품질관리 dto 생성, 검색 후 행 생성

--- teamBootstrap/src/main/java/ksmart39/springboot/service/QualityInsMeasurementValueService.java
+++ teamBootstrap/src/main/java/ksmart39/springboot/service/QualityInsMeasurementValueService.java
@@ -1,6 +1,5 @@
 package ksmart39.springboot.service;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -10,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ksmart39.springboot.dao.QualityInsMeasurementValueMapper;
-import ksmart39.springboot.domain.QualityInspection;
 
 @Service
 public class QualityInsMeasurementValueService {
@@ -25,7 +23,7 @@ public class QualityInsMeasurementValueService {
 
 	
 	//품질검사 요청 검색 모달
-	public List<Map<String, Object>> searchQualityInspectionRequest(Map map){			
+	public List<Map<String, Object>> searchQualityInspectionRequest(Map<String,Object> map){			
 		return qualityInsMeasurementValueMapper.searchQualityInspectionRequest(map);
 	};
 }
