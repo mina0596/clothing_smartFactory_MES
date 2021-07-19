2c5affcbe52642d231a4cc2187ca9c7c84d57f61
v2one <v2one@DESKTOP-9EB2744>
2021-07-19 23:55:31 +0900

    [TEAM01]

--- teamBootstrap/src/main/java/ksmart39/springboot/controller/SystemController_LHB.java
+++ teamBootstrap/src/main/java/ksmart39/springboot/controller/SystemController_LHB.java
@@ -39,23 +39,24 @@ public class SystemController_LHB {
 	private static final Logger log = LoggerFactory.getLogger(SystemController_LHB.class);
 
 
-	
-	//수정화면 ->목록
-	@PostMapping("modifyHumanResources")
-	public String modifyHumanResources(HumanResources humanResources) {
-		systemService.modifyHumanResources(humanResources);
-		return "redirect:/humanResourcesList";
+	//[한빛]사원등록
+	@GetMapping("/addHumanResources")
+	public String addHumanResources(Model model) {
+		model.addAttribute("title", "인사관리");
+		return"system/addHumanResources";	
 	}
 	
-	//[한빛]사원수정
-	@GetMapping("/modifyHumanResources")
-	public String modifyHumanResources(@RequestParam(name = "employeeCode", required = false) String employeeCode, Model model) {
-		//1. 회원코드로 회원테이블을 조회한 HumanResources객체
-		HumanResources humanResources = systemService.getEmployeeInfoByCode(employeeCode);
-		//2. Model 화면에 전달할 객체 삽입
-		model.addAttribute("title","회원수정폼");
-		model.addAttribute("humanResources",humanResources);
-		return "system/modifyHumanResources";
+	@PostMapping("/memberIdCheck")
+	@ResponseBody
+	public boolean memberIdCheck(@RequestParam(value = "employeeId") String employeeId) {
+		
+		boolean idCheck = true;
+		
+		HumanResources humanResources = systemService.getEmployeeInfoById(employeeId);
+		
+		if(humanResources != null) idCheck = false;
+		
+		return idCheck;
 	}
 	
 	//[한빛]사원등록-> 사원목록
@@ -65,16 +66,10 @@ public class SystemController_LHB {
 		
 		return "redirect:/humanResourcesList";
 	}
-	//[한빛]사원등록
-	@GetMapping("/addHumanResources")
-	public String addHumanResources(Model model) {
-		model.addAttribute("title", "인사관리");
-		return"system/addHumanResources";	
-	}
 	
 	//[한빛]사원목록
 	@GetMapping("/humanResourcesList")
-	public String humanResourcesList(Model model
+	public String getHumanResourcesList(Model model
 									,@RequestParam(name="searchKey",required = false) String searchKey
 									,@RequestParam(name="searchValue",required=false) String searchValue) {
 		
@@ -94,20 +89,23 @@ public class SystemController_LHB {
 		return "system/humanResourcesList";
 	}
 
-	@PostMapping("/memberIdCheck")
-	@ResponseBody
-	public boolean memberIdCheck(@RequestParam(value = "employeeId") String employeeId) {
-		
-		boolean idCheck = true;
-		
-		HumanResources humanResources = systemService.getEmployeeInfoById(employeeId);
-		
-		if(humanResources != null) idCheck = false;
-		
-		return idCheck;
+	//[한빛]사원수정
+	@GetMapping("/modifyHumanResources")
+	public String modifyHumanResources(@RequestParam(name = "employeeCode", required = false) String employeeCode, Model model) {
+		//1. 회원코드로 회원테이블을 조회한 HumanResources객체
+		HumanResources humanResources = systemService.getEmployeeInfoByCode(employeeCode);
+		//2. Model 화면에 전달할 객체 삽입
+		model.addAttribute("title","회원수정폼");
+		model.addAttribute("humanResources",humanResources);
+		return "system/modifyHumanResources";
 	}
 	
-
+	//수정화면 ->목록
+	@PostMapping("modifyHumanResources")
+	public String modifyHumanResources(HumanResources humanResources) {
+		systemService.modifyHumanResources(humanResources);
+		return "redirect:/humanResourcesList";
+	}
 
 
 	// ==============================================================
@@ -125,6 +123,21 @@ public class SystemController_LHB {
 		return "redirect:/clientList";
 	}
 	
+	//[한빛]거래처 조회
+	@GetMapping("/clientList")
+	public String getClientList(@RequestParam(name = "searchKey", required = false) String searchKey,
+													@RequestParam(name = "searchValue", required = false) String searchValue, 
+																					Model model) {
+		Map<String, Object> paramMap = new HashMap<String,Object>();
+		paramMap.put("searchKey", searchKey);
+		paramMap.put("searchValue", searchValue);
+
+		List<Client> client = systemService.getClient(paramMap);
+		model.addAttribute("title", "거래처관리");
+		model.addAttribute("client", client);
+		return "system/clientList";
+	}
+
 	//[한빛]거래처수정
 	@GetMapping("/modifyClient")
 	public String modifyClient(@RequestParam(name = "clientCode", required = false) String clientCode, Model model) {
@@ -143,22 +156,7 @@ public class SystemController_LHB {
 		log.info("=================");
 		log.info("{}",client);
 		return "redirect:/clientList";
-	}		
-	
-	//[한빛]거래처 조회
-	@GetMapping("/clientList")
-	public String getClientList(@RequestParam(name = "searchKey", required = false) String searchKey,
-													@RequestParam(name = "searchValue", required = false) String searchValue, 
-																					Model model) {
-		Map<String, Object> paramMap = new HashMap<String,Object>();
-		paramMap.put("searchKey", searchKey);
-		paramMap.put("searchValue", searchValue);
-
-		List<Client> client = systemService.getClient(paramMap);
-		model.addAttribute("title", "거래처관리");
-		model.addAttribute("client", client);
-		return "system/clientList";
-	}
+	}			
 
 	//[한빛]거래처 삭제
 	@PostMapping("/deleteClient")
