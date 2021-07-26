7b82814584f8058215a2885e2884c437aa736641
Minah Park <mina0596@hotmail.com>
2021-07-26 13:56:51 +0900

    뭐지...?

--- teamBootstrap/src/main/java/ksmart39/springboot/service/QualityControlService.java
+++ teamBootstrap/src/main/java/ksmart39/springboot/service/QualityControlService.java
@@ -1,6 +1,7 @@
 package ksmart39.springboot.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,17 @@ public class QualityControlService {
 	@Autowired
 	private QualityControlMapper qualityControlMapper;
 	
-	//[보람 ]품질검사 리스트 조회메서드
-		public List<QualityInspection> getQualityInspectionList(){
-			List<QualityInspection> qualityInspectionList = qualityControlMapper.getQualityInspectionList();
-		
-			return   qualityInspectionList;
-		}
+	//[보람]품질검사 리스트 조회메서드
+	public List<QualityInspection> getQualityInspectionList(){
+		List<QualityInspection> qualityInspectionList = qualityControlMapper.getQualityInspectionList();
+	
+		return   qualityInspectionList;
+	}
+	
+	//[민아]품질검사 기준표 리스트
+	public List<Map<String,Object>> getInspectionStandard(String inspectionCate){
+		return qualityControlMapper.getInspectionStandard(inspectionCate);
+	}
 		
 	
 }
