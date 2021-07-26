bedd56929ca68790ddfe9f337ded24f9e09230ae
ECS <ECS@DESKTOP-048HU3O>
2021-07-26 13:56:48 +0900

    [team01]품질기초관리 최종 마무리 및 계약별 검색 1차 완료 author:한보람 file:teamBootstrap/src/main/java/ksmart39/springboot/controller/QualityControlController_HBR.java teamBootstrap/src/main/java/ksmart39/springboot/dao/QualityInsepctionStauteMapper.java teamBootstrap/src/main/resources/mapper/QualityInsepctionStauteMapper.xml teamBootstrap/src/main/java/ksmart39/springboot/service/QualityInsepctionStauteService.java teamBootstrap/src/main/resources/templates/system/qualityInspectionList.html teamBootstrap/src/main/resources/templates/quality/qualityInspectionStatusContract.html teamBootstrap/src/main/resources/static/quality/qualityInspectionStatusContract.js

--- teamBootstrap/src/main/resources/static/quality/qualityInspectionStatusContract.js
+++ teamBootstrap/src/main/resources/static/quality/qualityInspectionStatusContract.js
@@ -4,21 +4,26 @@
 $(function(){
 	$('.searchInspectionResult').hide();
 	
+	var bySupplierValue = $('#bySupplierValue');
+	var clientName =$('#clientName');
+
+	
 	$('#searchBtn1').click(function(){
+		
+			
+			
 		var html='';
 		var array={
 				bySupplierValue:$('#bySupplierValue').val()
 				,clientName:$('#clientName').val()
+				,requestProductName:$('#requestProductName').val()
 				,inspectionStartDate:$('#inspectionStartDate').val()
 				,inspectionEndDate:$('#inspectionEndDate').val()
-				,requestProduct:$('#requestProduct').val()
-				,InspectionCode:$('#inspectionCode').val()
-				,inspectionClassName:$('#inspectionClassName').val()
 		};
 		console.log(array)
 		var request = $.ajax({
 			url: "/quality/searchQualityInspection",
-			method: "get"
+			method: "get",
 			type: "post",
 			data: array,
 			dataType: "json"	
@@ -26,7 +31,34 @@ $(function(){
 		}); 
 		request.done(function( data ) {
 			$('.searchInspectionResult').show();
-		
+			$('.stateContractInspectionList').hide();
+			$('#searchContractInspectionList' ).empty();
+			 if(data.length > 0){
+			        for(i=0; i<data.length; i++){
+			        	html +="<tr>";
+			        	html += "<th scope='row' id='mandatory'>"+[i + 1]+"</th>";
+			        	html += "<td>"+ data[i].highClassName +"</td>";
+			        	html += "<td>"+ data[i].lowClassName +"</td>";
+			        	html += "<td>"+ data[i].subClassName +"</td>";
+			        	html += "<td>"+ data[i].requestProductCode +"</td>";
+			        	html += "<td>"+ data[i].detailedCateName +"</td>";
+			        	html += "<td>"+ data[i].inspectionRequestDate +"</td>";
+			        	html += "<td>"+ data[i].inspectionCategory +"</td>";
+			        	html += "<td>"+ data[i].standardTolerance +"</td>";
+			        	html += "<td>"+ data[i].minTolerance +"</td>";
+			        	html += "<td>"+ data[i].maxTolerance +"</td>";
+			        	html += "<td>"+ data[i].insFirst +"</td>";
+			        	html += "<td>"+ data[i].insSecond +"</td>";
+			        	html += "<td>"+ data[i].insThird +"</td>";
+			        	html += "<td>"+ data[i].insStart +"</td>";
+			        	html += "<td>"+ data[i].insEnd +"</td>";
+			        	html += "<td>"+ data[i].finalPassCheck +"</td>";
+			        	html +="</tr>";
+			        }	
+			      } else{
+			        html += "<tr><td colspan='17' style='text-align: center;'> 검색된 결과가 없습니다. </td></tr>";
+			        }
+			       $('#searchContractInspectionList').append(html);
 		});
 		request.fail(function( jqXHR, textStatus ) {
 			alert( "Request failed: " + textStatus );
