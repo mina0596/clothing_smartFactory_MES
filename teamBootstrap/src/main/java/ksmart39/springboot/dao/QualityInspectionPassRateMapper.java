61c601b02f9509003046795c55ebb504cdd9c965
Minah Park <mina0596@hotmail.com>
2021-08-02 02:40:58 +0900

    [team01]민아
    연도별 불량률 그래프와 그 안의 데이터 테이블완성

--- teamBootstrap/src/main/java/ksmart39/springboot/dao/QualityInspectionPassRateMapper.java
+++ teamBootstrap/src/main/java/ksmart39/springboot/dao/QualityInspectionPassRateMapper.java
@@ -12,4 +12,11 @@ public interface QualityInspectionPassRateMapper {
 	//[민아]불량률높은 5위 조회
 	public List<Map<String,Object>> getInspectionFailedRank();
 	
+	//[민아]연도별 불량률 조회
+	public Map<String,Object> getPastYearsFailedPercent();
+	
+	//[민아]연도별 불량률 5위 상세정보
+	public List<Map<String,Object>> getYearlyFailRank(String inputYear);
+	
+	
 }
